c41c088fea97a874260352312e5bd513a6cca951
Alejandro Reyna <AleejandroReyna@gmail.com>
2019-11-15 18:41:45 -0600

    esteban terminó el primer ejercicio

--- ejercicio-01.py
+++ ejercicio-01.py
@@ -1,1 +1,11 @@
-sdfgsdfgsdfgsdfgsfdgsfdgsdfgsfdgsfdgfd
+print("cual es tu nombre?")
+nombre = input()
+print("cual es tu apellido?")
+apellido = input()
+print("cual es tu edad?")
+edad = int(input())
+
+if edad >= 18:
+    print("Hola " + nombre + " " + apellido + " eres mayor de edad")
+else:
+    print("Hola " + nombre + " " + apellido + " eres menor de edad")
